eb4a64e8b6ca01b4b6f61cd19e15f4b75ed2be3c
lukehayes <lukepurplehayes@gmail.com>
2021-09-22 17:48:45 +0100

    Moved logic from index to PTask\PTask class.

--- index.php
+++ index.php
@@ -2,47 +2,5 @@
 <?php
 require "vendor/autoload.php";
 
-use PTask\Formatter;
-
-$formatter = new Formatter();
-$file = "todo.txt";
-
-function parseArguments($args)
-{
-    global $argc;
-    $flags = [];
-
-    if ($argc <= 2) {
-        echo "No additonal arguments are present.\n"; 
-        return;
-    }
-
-    array_push($flags, $args[1]);
-    array_push($flags, $args[2]);
-    
-    return $flags;
-}
-
-$flags = parseArguments($argv);
-
-switch ($flags[0]) {
-    case "-ls":
-        echo "Listing All Tasks...\n";
-        break;
-    case "-a":
-        echo "Adding Tasks.\n";
-        break;
-    case "-d":
-        echo "Marking Task As Done.\n";
-        break;
-    case "-clear":
-        $formatter->line();
-        echo "CLEARING ALL TASKS.\n";
-        $formatter->line();
-        break;
-    
-    default:
-        echo "Nothing To Be Done.\n";
-        break;
-}
-
+$PTask = new PTask\PTask();
+$PTask->run($argc, $argv);
---
+++ src/PTask.php
@@ -0,0 +1,60 @@
+<?php
+namespace PTask;
+
+/**
+ * Main entry point for this application.
+ */
+class PTask
+{
+    /**
+     * Start the application.
+     */
+    public function run($argc, $argv)
+    {
+        if ($argc <= 2) {
+            echo "No additonal arguments are present.\n"; 
+            return;
+        }
+    }
+
+    /**
+     * Parse the arguments from the command line.
+     *
+     * @return array
+     */
+    function parseArguments($args) : array
+    {
+        $flags = [];
+        array_push($flags, $args[1]);
+        array_push($flags, $args[2]);
+        return $flags;
+    }
+
+    /**
+     * Run the appropiate task based on flag input.
+     */
+    public function runTask($task)
+    {
+        switch ($task[0]) {
+            case "-ls":
+                echo "Listing All Tasks...\n";
+                break;
+            case "-a":
+                echo "Adding Tasks.\n";
+                break;
+            case "-d":
+                echo "Marking Task As Done.\n";
+                break;
+            case "-clear":
+                $formatter->line();
+                echo "CLEARING ALL TASKS.\n";
+                $formatter->line();
+                break;
+            
+            default:
+                echo "Nothing To Be Done.\n";
+                break;
+        }
+
+    }
+}
