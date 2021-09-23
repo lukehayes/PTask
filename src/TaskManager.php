ff1bc60fa620d1e0384d3a23489301a8baf26bdb
lukehayes <lukepurplehayes@gmail.com>
2021-09-23 09:27:37 +0100

    PTask\TaskManager can now set its todolist file.

--- src/TaskManager.php
+++ src/TaskManager.php
@@ -7,14 +7,33 @@ namespace PTask;
 class TaskManager
 {
     /**
+     * @var string $todolist */
+    private $todolist = NULL;
+
+    public function __construct($file)
+    {
+        $this->todolist = $file;
+    }
+
+    /**
+     * Set the todolist file that is read and written to etc.
+     *
+     * @return void
+     */
+    public function setTodoList($file) : void
+    {
+        $this->todolist = $file;
+    }
+
+    /**
      * Read the entire contents of a file
      *
      * @param string $file    The file to be read.
      * @return string         The files contents.
      */
-    public function read($file) : string
+    public function read() : string
     {
-        return file_get_contents($file);
+        return file_get_contents($this->todolist);
     }
 }
 
